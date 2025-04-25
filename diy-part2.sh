baeec73b18ef4ac7bf010354dad04283fa1f59d8
wuyuqq <33753437+wuyuqq@users.noreply.github.com>
2025-04-25 17:08:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.20/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -24,16 +24,16 @@ sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 # 全能推送
-git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+#git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 
 # Mihomo TProxy
 #git clone https://github.com/morytyann/OpenWrt-mihomo.git package/mihomo
 
 # 调整 netdata 到 状态 菜单
-sed -i 's/"system"/"status"/g' feeds/luci/applications/luci-app-netdata/luasrc/controller/*.lua
+#sed -i 's/"system"/"status"/g' feeds/luci/applications/luci-app-netdata/luasrc/controller/*.lua
 
 # 调整 filebrowser 到 系统 菜单
-sed -i 's/services/system/g' feeds/luci/applications/luci-app-filebrowser/root/usr/share/luci/menu.d/*.json
+#sed -i 's/services/system/g' feeds/luci/applications/luci-app-filebrowser/root/usr/share/luci/menu.d/*.json
 
 # 调整 nlbwmon 到 一级 菜单
 #sed -i 's/services\///g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/*.js
