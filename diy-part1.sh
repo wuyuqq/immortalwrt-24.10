c358c463a2aa965766e4fa4c2922e64b1a7574e5
wuyuqq <33753437+wuyuqq@users.noreply.github.com>
2025-09-22 21:33:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 
 # Add mihomo tproxy source
 #echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
